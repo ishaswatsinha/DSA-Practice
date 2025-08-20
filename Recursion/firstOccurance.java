44cbab816c25d00202e170923c01e0cd8b57f620
Shaswat <shaswatsinha87@gmail.com>
2025-08-20 14:57:47 +0530

    lastOccurance

--- Recursion/firstOccurance.java
+++ Recursion/firstOccurance.java
@@ -12,11 +12,22 @@ public class firstOccurance {
         return firstOccur(arr, key, i+1);
 
     }
+    public static int lastOccur(int arr[],int key, int i){
+        
+        if (i==0) {
+            return -1;
+        }
+        if (arr[i]==key) {
+            return i;
+        }
+        return lastOccur(arr, key, i-1);
+    }
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5};
-        int key = 30;
-        int i = 0;
-        System.out.println(firstOccur(arr, key, i));
+        int arr[] = {1,2,1,5,3,4,5};
+        int key = 68;
+        int i = arr.length-1;
+        System.out.println(firstOccur(arr, key, 0));
+        System.out.println(lastOccur(arr, key, i));
     }
     
 }
